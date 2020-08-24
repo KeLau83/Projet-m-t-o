8cb744b37f74535eb7d1aad1a3facffcbca8ae1f
KeLau83 <stings83136@gmail.com>
2020-08-24 11:46:28 +0200

    refactoré à moitié

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ let favoris = [];
 const userInfo = {
     city: "Paris",
 }
+
 function creationPage() {
     userInfo.city = getCity()
     let dataMeteo = "https://www.prevision-meteo.ch/services/json/" + userInfo.city;
     fetch(dataMeteo)
         .then(response => response.json())
         .then(function (response) {  
-            let conditionAct = response.current_condition.condition
-            HTMLMainInfo(response,conditionAct)
+            let currentCondition = response.current_condition.condition
+            HTMLMainInfo(response,currentCondition)
             HTMLTitle()
-            setBackground(conditionAct)
+            setBackground(currentCondition)
             setHourByHour(response)
-            nextDays(response)
+            setNextDays(response)
             HTMLinfoSup(response)
             addFunctionToNewFav()
             hideInfo()
@@ -27,6 +28,7 @@ function creationPage() {
             alert("Erreur")
         })
 }
+
 function getCity() {
     if ($cityChoose.value != "") {
         return $cityChoose.value;
@@ -35,15 +37,64 @@ function getCity() {
     }
 }
 
-function HTMLMainInfo (response, conditionAct) {
-    document.querySelector(".tempAct").innerHTML = `${response.current_condition.tmp}°C`;
-    document.querySelector(".logoTemp").innerHTML = `<img src=${response.current_condition.icon_big}>`;
-    document.querySelector(".condition").innerHTML = conditionAct;
+function HTMLMainInfo (dataWeather, currentCondition) {
+    document.querySelector(".tempAct").innerHTML = `${dataWeather.current_condition.tmp}°C`;
+    document.querySelector(".logoTemp").innerHTML = `<img src=${dataWeather.current_condition.icon_big}>`;
+    document.querySelector(".condition").innerHTML = currentCondition;
 }
 
 function HTMLTitle() {
     let cityFav = compareCityToFavoris()
     HTMLBtnFav(cityFav)
+}
+
+function setBackground(currentCondition) {
+    let skyTint = currentCondition.substr(0, 4)
+    if (skyTint == "Nuit") {
+        $body.classList.add("nuit");
+
+    } else {
+        $body.classList.add("day")
+    }
+}
+
+function setHourByHour(dataWeather) {
+    let currentHour = dataWeather.current_condition.hour
+    let hourKey = getHour(currentHour)
+    resetHTML(".hourByHour")
+    getWeatherHours(hourKey,dataWeather)
+}
+
+function setNextDays(dataWeather) {
+    resetHTML(".nextDays")
+    for (let i = 0; i < 5; i++) {
+        let futurDay = "fcst_day_" + i;
+        let dayWeather = dataWeather[futurDay];
+        HTMLForNextDays(dayWeather, i);
+    }
+}
+
+function HTMLinfoSup(dataWeather) {
+    document.querySelector(".humidity").innerHTML = "Le taux d'humidité est de : " + dataWeather.current_condition.humidity + "%";
+    document.querySelector(".windSpeed").innerHTML = "La vitesse du vent est de " + dataWeather.current_condition.wnd_spd + "km/h";
+    document.querySelector(".sunRise").innerHTML = "Le soleil se lévera à " + dataWeather.city_info.sunrise;
+    document.querySelector(".sunSet").innerHTML = "et se couchera à " + dataWeather.city_info.sunset;
+}
+
+function addFunctionToNewFav() {
+    const $dropddownFav = document.querySelectorAll(".dropdown-item")
+    for (item of $dropddownFav) {
+        item.addEventListener("click", newPage)
+    }
+}
+
+function  hideInfo() {
+    $infoSup.classList.remove("active")
+    $infoSup.classList.add("sup-info_disable");
+    const $lineInfo = document.querySelectorAll(".lineInfo");
+    for (div of $lineInfo) {
+        div.classList.add("displayNone")
+    }
 }
 
 function compareCityToFavoris () {
@@ -68,99 +119,56 @@ function HTMLBtnFav (cityFav) {
     }
 }
 
-function capitalize(string) {
-    if (typeof string !== 'string') return ''
-    return string.charAt(0).toUpperCase() + string.slice(1)
+function getHour(currentHour) {
+    let hourKey = 0;
+    let hourFormat = "";                        // variable qui va prendre les différentes heures nous permettant de comparer à l'heure actuelle
+    while (hourFormat != currentHour) {
+        if (hourKey < 10) {
+            iHour = "0" + hourKey + ":00";
+        } else if (hourKey >= 10) {
+            hourFormat = hourKey + ":00";
+        }
+        hourKey += 1;
+    }
+    return hourKey
 }
 
-function setBackground(conditionAct) {
-    let skyTint = conditionAct.substr(0, 4)
-    if (skyTint == "Nuit") {
-        $body.classList.add("nuit");
-
-    } else {
-        $body.classList.add("day")
+function getWeatherHours(i, dataWeather) {
+    let DayForTheHours = "fcst_day_0";
+    for (let j = 0; j < 24; i++, j++) {
+        if (i == 24) {
+            i = 0;
+            DayForTheHours = "fcst_day_1";
+        }
+        const pathHour = i + "H00"
+        const hourIcon = dataWeather[DayForTheHours].hourly_data[pathHour].ICON;
+        const hourTemp = dataWeather[DayForTheHours].hourly_data[pathHour].TMP2m;
+        HTMLForEachHour(pathHour, hourIcon, hourTemp)
     }
 }
 
-function setHourByHour(response) {
-    let hourAct = response.current_condition.hour
-    let i = searchActHour(hourAct)
-    resetHTML(".hourByHour")
-    getWeatherHours(i,response)
-}
-
-function nextDays(response) {
-    resetHTML(".nextDays")
-    for (let i = 0; i < 5; i++) {
-        let futurDay = "fcst_day_" + i;
-        
-        HTMLForNextDays(response, futurDay, i);
-    }
-}
-
-function HTMLForNextDays(response, futurDay, i) {
-    let day = knowIfItsToday(response, futurDay, i)
+function HTMLForNextDays(dayWeather, i) {
+    let day = knowIfItsToday(dayWeather, i)
     const div = document.createElement('div');
     div.className = "weather-next-days";
-    div.innerHTML = `<p>${day}</p> <p>${response[futurDay].tmin}/${response[futurDay].tmax}°C</p> <img src="${response[futurDay].icon}">`;
+    div.innerHTML = `<p>${day}</p> <p>${dayWeather.tmin}/${dayWeather.tmax}°C</p> <img src="${dayWeather.icon}">`;
     document.querySelector(".nextDays").appendChild(div);
 }
 
-function knowIfItsToday (response, futurDay, i) {
+function HTMLForEachHour(pathHour, hourIcon, hourTemp) {
+    const div = document.createElement('div');
+    div.className = "weather-by-hour";
+    div.innerHTML = `<p>` + pathHour + `</p> <img src="` + hourIcon + `"><p>` + hourTemp + ` °C</p>`;
+    document.querySelector(".hourByHour").appendChild(div);
+}
+
+function knowIfItsToday (dayWeather, i) {
     if (i == 0) {
         return "Auj.";
     }
-    return response[futurDay].day_short;
+    return dayWeather.day_short;
 }
-
-function HTMLinfoSup(response) {
-    document.querySelector(".humidity").innerHTML = "Le taux d'humidité est de : " + response.current_condition.humidity + "%";
-    document.querySelector(".windSpeed").innerHTML = "La vitesse du vent est de " + response.current_condition.wnd_spd + "km/h";
-    document.querySelector(".sunRise").innerHTML = "Le soleil se lévera à " + response.city_info.sunrise;
-    document.querySelector(".sunSet").innerHTML = "et se couchera à " + response.city_info.sunset;
-}
-
-function  hideInfo() {
-    $infoSup.classList.remove("active")
-    $infoSup.classList.add("sup-info_disable");
-    const $lineInfo = document.querySelectorAll(".lineInfo");
-    for (div of $lineInfo) {
-        div.classList.add("displayNone")
-    }
-}
-
-function addFunctionToNewFav() {
-    const $dropddownFav = document.querySelectorAll(".dropdown-item")
-    for (item of $dropddownFav) {
-        item.addEventListener("click", newPage)
-    }
-
-}
-
-function displayFav() {
-    document.querySelector("#navbarDropdownMenuLink").classList.add("displayNone")
-}
-
-function HTMLFav() {
-    resetHTML(".dropdown-menu");
-    let datFav = localStorage.getItem('datFav');
-    if (datFav == "") {
-        favoris = []
-    } else {
-        favoris = datFav.split(',');
-    }
-    for (favori of favoris) {
-        if (favori != "") {
-            const a = document.createElement('a');
-            a.className = "dropdown-item";
-            a.href = "#";
-            a.innerHTML = favori;
-            document.querySelector(".dropdown-menu").appendChild(a);
-        }
-    }
-}
-
+ 
 function addFav() {
     let cityAdd = getCityToAdd()
     getStorage()
@@ -194,6 +202,29 @@ function setCityToStorage(cityAdd) {
     resetHTML(".dropdown-menu")
 }
 
+function HTMLFav() {
+    resetHTML(".dropdown-menu");
+    let datFav = localStorage.getItem('datFav');
+    if (datFav == "") {
+        favoris = []
+    } else {
+        favoris = datFav.split(',');
+    }
+    for (favori of favoris) {
+        if (favori != "") {
+            const a = document.createElement('a');
+            a.className = "dropdown-item";
+            a.href = "#";
+            a.innerHTML = favori;
+            document.querySelector(".dropdown-menu").appendChild(a);
+        }
+    }
+}
+
+function setFavToStorage(){
+    localStorage.setItem('datFav', favoris)
+}
+
 function  removeFav() {
     let cityCompare = userInfo.city.toLowerCase()
     for (favori of favoris) {
@@ -208,47 +239,6 @@ function  removeFav() {
     addFunctionToNewFav()
 }
 
-function searchActHour(hourAct) {
-    let i = 0;
-    let iHour = "";                        // variable qui va prendre les différentes heures nous permettant de comparer à l'heure actuelle
-    while (iHour != hourAct) {
-        if (i < 10) {
-            iHour = "0" + i + ":00";
-        } else if (i >= 10) {
-            iHour = i + ":00";
-        }
-        i += 1;
-    }
-    return i
-}
-
-function getWeatherHours(i, response) {
-    
-    let DayForTheHours = "fcst_day_0";
-    for (let j = 0; j < 24; i++, j++) {
-        if (i == 24) {
-            i = 0;
-            DayForTheHours = "fcst_day_1";
-        }
-        const pathHour = i + "H00"
-        const hourIcon = response[DayForTheHours].hourly_data[pathHour].ICON;
-        const hourTemp = response[DayForTheHours].hourly_data[pathHour].TMP2m;
-        HTMLForEachHour(pathHour, hourIcon, hourTemp)
-    }
-}
-
-function HTMLForEachHour(pathHour, hourIcon, hourTemp) {
-    const div = document.createElement('div');
-    div.className = "weather-by-hour";
-    div.innerHTML = `<p>` + pathHour + `</p> <img src="` + hourIcon + `"><p>` + hourTemp + ` °C</p>`;
-    document.querySelector(".hourByHour").appendChild(div);
-}
-
-function newPage() {
-    userInfo.city = event.target.textContent;
-    creationPage()
-}
-
 function showInfo() {
     $infoSup.classList.remove("sup-info_disable");
     const $lineInfo = document.querySelectorAll(".lineInfo");
@@ -258,12 +248,24 @@ function showInfo() {
     }
 }
 
+function capitalize(string) {
+    if (typeof string !== 'string') return ''
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 function resetHTML(place) {
     document.querySelector(place).innerHTML = ""
 }
-function setFavToStorage(){
-    localStorage.setItem('datFav', favoris)
+
+function displayFav() {
+    document.querySelector("#navbarDropdownMenuLink").classList.add("displayNone")
 }
+
+function newPage() {
+    userInfo.city = event.target.textContent;
+    creationPage()
+}
+
 document.querySelector('.sup-info').addEventListener('click', showInfo)
 $cityChoose.addEventListener('change', creationPage);
 creationPage()
